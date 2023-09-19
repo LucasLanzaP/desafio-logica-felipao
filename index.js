7697829de9bfa72eb83734050855c3fe0c8134b9
LucasLanzaP <lanzadp@gmail.com>
2023-09-18 21:41:13 -0300

    Correção da estrutura de controle if do desafio de Lógica da Progamação do Felipão-DIO.

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
 let playerName = "Logic"
-let playerXp = 0
-
-switch (playerXp) {
-    case "< 1001":
-        console.log("Ferro")
-        break;
-    case "> 1000 || < 2001":
-        console.log("Bronze")
-        break;
-    case "> 2000 || < 3001":
-        console.log("Prata")
-        break;
-    case "> 3000 || < 4001":
-        console.log("Ouro")
-        break;
-    case "> 4000 || < 5001":
-        console.log("Platina")
-        break;
-    case "> 5000 || < 6001":
-        console.log("Épico")
-        break;
-    case "> 6000 || < 7001":
-        console.log("Ascendente")
-        break;
-    case "> 7000 || < 8001":
-        console.log("Imortal")
-        break;
-    case "> 8000 || < 9001":
-        console.log("Legendário")
-        break;
-    case "> 9000 || < 10001":
-        console.log("Mítico")
-        break;
-    case "> 10000":
-        console.log("Radiante")
-        break
-    
-}
+let playerXp = 10000 
+let playerTitle = ""
 
 
-console.log("O Herói de nome " + playerName + " está no nível " + playerXp )
+if (playerXp < 1000) {
+    playerTitle = "Ferro"
+} else if  (playerXp > 1000) {
+    playerTitle = "Bronze"
+} else if (playerXp > 2000) {
+    playerTitle = "Prata"
+} else if (playerXp > 3000) {
+    playerTitle = "Ouro"
+} else if (playerXp > 4000) {
+    playerTitle = "Platina"
+} else if (playerXp > 5000) {
+    playerTitle = "Épico"
+} else if (playerXp > 6000) {
+    playerTitle = "Ascendente"
+} else if (playerXp > 7000) {
+    playerTitle = "Imortal"
+} else if (playerXp > 8000) {
+    playerTitle = "Lendário"
+} else if (playerXp > 9000) {
+    playerTitle = "Mítico"
+} else (playerXp > 10000) 
+    playerTitle = "Radiante"
+
+
+console.log("O Herói de nome " + playerName + " está no nível " + playerTitle + ".")
